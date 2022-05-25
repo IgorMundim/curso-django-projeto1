400fa3885591a86be5aa5d0a02d21312626cdaf4
Igor Mundim <igormundim@github.com>
2022-05-24 22:21:59 -0300

    include ORM

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,11 @@
-
 from django.urls import path
-from recipes.views import home
+
+from . import views
+
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),
+    path('', views.home, name="home"),
+    path('category/<int:category_id>/', views.category, name="category"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,29 @@
-
-
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
-# Create your views here.
+from .models import Recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+
+def category(request,  category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True,
+    ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
