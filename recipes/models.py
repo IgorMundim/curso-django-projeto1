3f7b72815d2f91bde57d6a3ea985d69d13b295c5
Igor Mundim <igormundim@github.com>
2022-05-25 21:23:56 -0300

    test

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,3 @@
-
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -13,7 +12,7 @@ class Category(models.Model):
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
@@ -26,9 +25,12 @@ class Recipe(models.Model):
     cover = models.ImageField(
         upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
+    )
     author = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True)
+        User, on_delete=models.SET_NULL, null=True
+    )
 
     def __str__(self):
         return self.title
